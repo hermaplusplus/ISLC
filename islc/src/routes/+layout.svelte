63b68d75258adaa4fdb2cc00f6b340989286188c
herma <49307226+hermaplusplus@users.noreply.github.com>
2023-08-16 19:05:10 +0100

    Layout cleanup

--- islc/src/routes/+layout.svelte
+++ islc/src/routes/+layout.svelte
@@ -1,15 +1,10 @@
-
-
-<!--<div class="headernav">-->
 <nav>
-    <!--<img src="https://upload.wikimedia.org/wikipedia/sco/d/d2/Pizza_Hut_logo.svg" alt="PH logo" width="24pt" height="24pt"/>-->
     <a href="/">🍕</a>
     <a href="/">Home</a>
     <a href="/islc">ISLC</a>
     <a href="/banking">Banking</a>
     <a href="/more">More</a>
 </nav>
-<!--</div>-->
 
 <slot />
 
@@ -21,7 +16,7 @@
     <div style="text-align:left">
         <p>
             <a href="/disclaimer" target="_blank">Disclaimer</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="https://github.com/hermaplusplus/ISLC" target="_blank"><!--<img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub Logo" width="24pt" height="24pt"/>-->Source Code</a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="https://github.com/hermaplusplus/ISLC" target="_blank">Source Code</a>&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="https://github.com/hermaplusplus/ISLC/issues" target="_blank">Report Issue</a>
         </p>
     </div>
@@ -39,40 +34,4 @@
       box-shadow: 0 0 .3em #999;
       background: #FFFA;
     }
-
-    .popup {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-    }
-
-    .popup h1 {
-      font-size: 2rem;
-      text-align: center;
-      color: red;
-    }
-
-    /*.headernav {
-      position: fixed;
-      left: 0;
-      top: 0;
-      width: 100%;
-    }
-
-    nav {
-        border-radius: 0 0 0 0;
-        box-shadow: 0 0 .3em #999;
-        background: #c51b29aa;
-    }
-
-    .header {
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      width: 100%;
-      padding: 1em;
-      border-radius: 2em 2em 0 0;
-      background: #B92C34;
-    }*/
 </style>
