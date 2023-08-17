2f4f730166cdab9fc817dff4a35d616596d9abbc
herma <49307226+hermaplusplus@users.noreply.github.com>
2023-08-17 20:17:40 +0100

    Small fix

--- islc/src/routes/+page.svelte
+++ islc/src/routes/+page.svelte
@@ -6,4 +6,4 @@
 <p>You can find some guides related to payment terminals under <a href="/banking">Banking</a>.</p>
 <p>More tools and guides can be found under <a href="/more">More</a>.</p>
 <p><div style="color:#e73841;"><i>Note: This site is designed for mobile users and is not optimized for desktop viewing (page elements may be scaled incorrectly)!</i></div>
-<p><div style="color:#e73841;"><i>Note: This site will be changing domains soon. This URL will redirect to the new one for a short while.</i></div>
+<p><div style="color:#e73841;"><i>Note: This site may be changing domains soon. This URL will redirect to the new one for a short while.</i></div>
