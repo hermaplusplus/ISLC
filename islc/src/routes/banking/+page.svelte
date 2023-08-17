0b52e0f56ce0a29ababd525dd98c0802dc8014c7
herma <49307226+hermaplusplus@users.noreply.github.com>
2023-08-17 20:35:13 +0100

    Fix

--- islc/src/routes/banking/+page.svelte
+++ islc/src/routes/banking/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Banking</h1>
 <p>As not all stores have the same banking setup (e.g. number of tills), this site will not cover the procedures done in Aztec.</p>
-<p>Below are links to guides for X/Z-Totals and End of Day banking for some card terminals.</p>
+<p>Below are links to guides for taking payments on some card terminals.</p>
 <a href="/banking/dx8000">
     <div style="width: fit-screen; padding: 1em; background: #ffffff; border-radius: var(--border-radius); box-shadow: 0 0 .3em #999;">
         <h3><b>WorldPay DX8000</b></h3>
